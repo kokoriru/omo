0a94b88233be28136b91cfb02a34582d35923c46
kokoriru <yuka@ryukalice.com>
2022-07-08 00:36:46 +0900

    マスタープロダクトの各種アクションを空で追加

---
+++ app/controllers/master/products_controller.rb
@@ -0,0 +1,15 @@
+class Master::ProductsController < ActionController::Base
+  def index; end
+
+  def show; end
+
+  def new; end
+
+  def create; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+end
