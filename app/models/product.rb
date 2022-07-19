358a3422fd410968cd27906c0d750d9b5fc179e1
kokoriru <yuka@ryukalice.com>
2022-07-20 02:12:53 +0900

    Product モデルと ProductCategory モデルと Category モデルの関連を追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :product_categories, dependent: :destroy
+  has_many :categories, through: :product_categories
+
   validates :name, presence: true
 
   enum status: { 'ほしい' => 0, '予約中' => 1, '完了' => 2, '保留' => 3, 'いらない' => 4 }
